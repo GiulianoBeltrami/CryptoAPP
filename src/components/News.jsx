ef60cb0a2c9d3a9fa4ff7dc60fcc250525cc2a01
Giuliano Beltrami <juka2027@live.com>
2021-12-08 18:29:06 -0300

    Continuação do componente news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -9,35 +9,58 @@ const {Option} = Select;
 const News = ({ simplified }) => {
  
     const count = simplified ? 6 : 12;
-    const { data:cryptoNews} = useGetCryptoNewsQuery( { newsCategory:'Cryptocurrency',count:count}  );
+    const { data:cryptoNews } = useGetCryptoNewsQuery( { newsCategory:'Cryptocurrency',count:count}  );
     const [newsList,setNews] = useState(cryptoNews?.value);
+    const [searchTerm,setSearchTerm] = useState("");
 
-    if (!cryptoNews.value) {
-         return "Loading..."
+    if (!newsList) {
+        return "Loading..."
     }
 
     return (
         <Row gutter={[24,24]}>
-            {renderCardForEachNews(newsList)}
-            {/* <Row gutter={[32,32]} className="crypto-card-container">
-                { renderCardForEachCurrency(cryptos) }
-            </Row> */}
+            {!simplified && (
+                <Col span={24}>
+                    <Select
+                        showSearch
+                        className="select-news"
+                        placeholder="Selecione uma criptomoeda"
+                        optionFilterProp="children"
+                        onChange={(event) => setSearchTerm(event.target.value)}
+                        filterOption={(input,option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0 }
+                    >
+
+                    </Select>
+                </Col>
+
+            )}
+            { renderCardForEachNews(newsList) }
         </Row>
     )
 }
 
 const renderCardForEachNews = (newsList) => {
-     return newsList?.map( (news,arrayIndex) =>(
-         <Col xs={24} sm={12} lg={8} key={arrayIndex}>
-             
+
+    const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
+
+    return newsList?.map( (news,arrayIndex) =>(
+        <Col xs={24} sm={12} lg={8} key={arrayIndex}>
             <Card hoverable className="news-card">
                 <a href={news.url} target="_blank" rel="noreferrer">
                     <div className="news-image-container">
                         <Title className="news-title" level={4}>{news.name}</Title>
+                        <img style={{maxWidth:'200px',maxHeight:'100px'}} src={news?.image?.thumbnail?.contentUrl || demoImage} alt="news" />
                     </div>
+                    <p>
+                        {news?.description > 100 ? `${news?.description.substring(0,100)}...`: news?.description }
+                    </p>
+                    <div className="provider-container">
+                        <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt="" />
+                        <Text className="provider-name">{news?.provider[0]?.name}</Text>
+                    </div>
+                    <Text>{moment(news?.datePublished).startOf('ss').fromNow()}</Text>
                 </a>
             </Card>
-
         </Col>
     ))
 }
